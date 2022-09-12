b7afa0e8dfa143326a1bb2da2c6837ea963c49c7
Furkanayaan <ayanf16@itu.edu.tr>
2022-09-12 17:03:18 +0300

    fibonacci

---
+++ Solution1/source/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace source
+{
+    public class Class1
+    {
+        
+        
+
+        public static void Main()
+        {
+            
+            int count_limit = 10;
+            
+            for (int i = 1; i < count_limit; i++)
+            {
+                for (int k = 0 ; k < count_limit-i; k++)
+                {
+                    Console.Write(" ");
+                }
+                
+
+                for (int j = 0; j < 2*i-1 ; j++)
+                {
+
+                    if (i - j <= 0)
+                    {
+                        Console.Write(j-i+2);
+                    }
+                    else
+                    {
+                        Console.Write(i - j);
+                    }
+                    
+                }
+
+                Console.Write("\n");
+            }
+            
+
+            
+            
+
+        } 
+        
+    }
+    
+}
